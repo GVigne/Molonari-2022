31ae767a5e95549dfb77dfa79c0ef7e02ef46927
Chengfeng GU <chengfenggu2000@gmail.com>
2022-04-06 17:51:04 +0200

    IHM1_F10

--- Molonari_2021/ihm/molonaviz/dialogcleanup.py
+++ Molonari_2021/ihm/molonaviz/dialogcleanup.py
@@ -13,10 +13,15 @@ class DialogCleanup(QtWidgets.QDialog, From_DialogCleanup):
         
         self.setupUi(self)
 
+        # Read the sample_text.txt
+        with open(os.path.join(os.path.dirname(__file__),"saved_text.txt")) as f:
+            sample_text = f.read()
+        # Set sample_text.txt as the defaut text on plainTextEdit
+        self.plainTextEdit.setPlainText(sample_text)
+
 
     def getScript(self):
         scriptpartiel = self.plainTextEdit.toPlainText()
         scriptindente = scriptpartiel.replace("\n", "\n   ")
         script = "def fonction(dft, dfp): \n   " + scriptindente + "\n" + "   return(dft, dfp)"
-        return(script)
-        
+        return(script,scriptpartiel)
